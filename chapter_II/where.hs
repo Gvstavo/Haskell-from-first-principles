21cc33fb88d783b694c5010c6734e8bcb9b2e1af
Gvstavo <gustavopaivads@gmail.com>
2021-08-15 12:13:59 -0300

    first commit

---
+++ chapter_II/where.hs
@@ -0,0 +1,14 @@
+module FunctionWithWhere where 
+
+ a = x * 3 + y 
+  where x = 3 
+        y = 1000
+
+ b = x * 5
+  where y = 10 
+        x = 10 * 5 + y
+
+ c = z / x + y
+  where x = 7
+        y = negate x 
+        z = y *10
